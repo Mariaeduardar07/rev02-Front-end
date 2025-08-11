a341bb10152b83e5e62e7c952772bd3574ff899f
Maria Eduarda <mariaeduardareisTDS2.senai@gmail.com>
2025-08-11 16:07:29 -0300

    feat: project finalized

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,5 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "../components/Header/Header"
-import { productsData } from "../../public/data/Products";
 
 // Configurar fonte local baixada
 const cormorantGaramond = localFont({
@@ -31,10 +29,6 @@ export default function RootLayout({ children }) {
     return (
         <html lang="pt-BR" className={cormorantGaramond.variable}>
             <body className={cormorantGaramond.className}>
-                <Header
-                title={ "Loja de Eletrônicos"}
-                subtitle={"Os melhores produtos com os melhores preços!"}
-                totalProdutos={productsData.length} />
                 <main>{children}</main>
             </body>
         </html>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,30 @@
-
 "use client";
 import { useEffect, useState } from "react";
-import ProductCard from "@/components/ProductCard/ProductCard";
+import ProductCard from "../components/ProductCard/ProductCard";
 import styles from "./page.module.css";
-import { productsData } from "../../public/data/Products.js";
+import { productsData } from "../../public/data/Products";
+import Header from "../components/Header/Header";
 
 export default function Home() {
-  const [ products, setProducts ] = useState([]);
+const [products, setProducts] = useState(productsData);
 
-  useEffect(() => {
-    setProducts(productsData);
-  }, []);
+  // useEffect(() => {
+  //   setProducts(productsData);
+  // }, []);
 
   return (
-    <div className={styles.products}>
-       {products.map((product) => (
-         <ProductCard key={product.id} product={product} />
-       ))}
+    <div className={styles.container}>
+      <Header
+        title={"Loja de Eletrônicos"}
+        subtitle={"Os melhores produtos com os melhores preços!"}
+        totalProducts={products.length}
+      />
+
+      <div className={styles.products}>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 }
